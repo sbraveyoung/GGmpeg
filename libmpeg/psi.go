7c8337454e74771b5caa1e97bdf456d927791dd4
sbrave <SmartBraveCoder@gmail.com>
2021-10-27 22:19:19 +0800

    unit test for psi

--- libmpeg/psi.go
+++ libmpeg/psi.go
@@ -1,6 +1,7 @@
-package mpeg
+package libmpeg
 
 import (
+	"encoding/binary"
 	"fmt"
 
 	"github.com/SmartBrave/Athena/easyio"
@@ -25,6 +26,7 @@ type PAT struct { //Program Association Table, PID:0x0000
 	SectionNumber        uint8 //8bit
 	LastSectionNumber    uint8 //8bit
 	PMTs                 map[uint16]*PMT
+	CRC32                uint32
 }
 
 func NewPAT() (pat *PAT) {
@@ -34,19 +36,20 @@ func NewPAT() (pat *PAT) {
 }
 
 func (pat *PAT) Parse(reader easyio.EasyReader) (err error) {
-	b, err := reader.ReadN(8)
+	b1 := make([]byte, 8)
+	err = reader.ReadFull(b1)
 	if err != nil {
 		return fmt.Errorf("read PAT error:%w", err)
 	}
 
-	pat.TableID = b[0]                                      //0x00
-	pat.SectionSyntaxIndicator = (b[1] >> 7) & 0x01         //0x01
-	pat.SectionLength = uint16(((b[1] & 0x0f) << 4) + b[2]) //0x0d == 13
-	pat.TransportStreamID = uint16(b[3]<<8 + b[4])          //0x01
-	pat.VersionNumber = (b[5] >> 1) & 0x1f                  //0x01
-	pat.CurrentNextIndicator = b[5] & 0x01                  //0x01
-	pat.SectionNumber = b[6]                                //0x00
-	pat.LastSectionNumber = b[7]                            //0x00
+	pat.TableID = b1[0]
+	pat.SectionSyntaxIndicator = (b1[1] & 0x80) >> 7
+	pat.SectionLength = uint16(b1[1]&0x0f)<<8 + uint16(b1[2])
+	pat.TransportStreamID = uint16(b1[3])<<8 + uint16(b1[4])
+	pat.VersionNumber = (b1[5] & 0x3e) >> 1
+	pat.CurrentNextIndicator = b1[5] & 0x01
+	pat.SectionNumber = b1[6]
+	pat.LastSectionNumber = b1[7]
 
 	if pat.TableID != 0x00 {
 		return fmt.Errorf("invalid TableID of PAT, TableID:%d", pat.TableID)
@@ -58,25 +61,32 @@ func (pat *PAT) Parse(reader easyio.EasyReader) (err error) {
 		return fmt.Errorf("invalid SectionLength of PAT, SectionLength:%d", pat.SectionLength)
 	}
 
-	b, err = reader.ReadN(uint32(pat.SectionLength - 5 - 4)) //4:CRC_32
+	b2 := make([]byte, pat.SectionLength-5-4) //4:CRC_32
+	err = reader.ReadFull(b2)
 	if err != nil {
 		return fmt.Errorf("read ProgramNUmber of PAT error:%w", err)
 	}
-	for i := 0; i+4 <= len(b); i += 4 { //167
-		programNumber := uint16(b[i]<<8 + b[i+1])
+	for i := 0; i+4 <= len(b2); i += 4 {
+		programNumber := uint16(b2[i])<<8 + uint16(b2[i+1])
 		if programNumber == 0x0000 { //NIT, ignore
 			continue
 		} else { //PMT
-			pmtPID := uint16(b[i+2]<<8 + b[i+3])
+			pmtPID := uint16(b2[i+2]&0x1f)<<8 + uint16(b2[i+3])
 			if _, ok := pat.PMTs[pmtPID]; !ok {
 				pat.PMTs[pmtPID] = NewPMT(programNumber)
 			}
 		}
 	}
 
-	b, err = reader.ReadN(4) //CRC_32
+	pat.CRC32 = CRC32(append(b1, b2...))
+
+	b3 := make([]byte, 4)
+	err = reader.ReadFull(b3) //CRC_32
 	if err != nil {
 		return fmt.Errorf("read CRC_32 of PAT error:%w", err)
+	}
+	if got := binary.BigEndian.Uint32(b3); got != pat.CRC32 {
+		return fmt.Errorf("crc error, want:%d, got:%d", pat.CRC32, got)
 	}
 	return nil
 }
@@ -84,20 +94,22 @@ func (pat *PAT) Parse(reader easyio.EasyReader) (err error) {
 func (pat *PAT) Marshal(writer easyio.EasyWriter) (err error) {
 	b := []byte{
 		pat.TableID,
-		((pat.SectionSyntaxIndicator & 0x01) << 7) | uint8((pat.SectionLength>>8)&0x0f),
+		((pat.SectionSyntaxIndicator << 7) & 0x80) | 0x30 | uint8((pat.SectionLength>>8)&0x0f),
 		uint8(pat.SectionLength & 0xff),
 		uint8((pat.TransportStreamID >> 8) & 0xff),
 		uint8(pat.TransportStreamID & 0xff),
-		((pat.VersionNumber & 0x1f) << 1) | (pat.CurrentNextIndicator & 0x01),
+		0xc0 | ((pat.VersionNumber << 1) & 0x37) | (pat.CurrentNextIndicator & 0x01),
 		pat.SectionNumber,
 		pat.LastSectionNumber,
 	}
 	for pmtPID, pmt := range pat.PMTs {
 		b = append(b, uint8(pmt.ProgramNumber>>8)&0xff, uint8(pmt.ProgramNumber&0xff))
-		b = append(b, uint8(pmtPID>>8)&0xff, uint8(pmtPID&0xff))
+		b = append(b, 0xe0|uint8((pmtPID>>8)&0xff), uint8(pmtPID&0xff))
 	}
-	//TODO: CRC_32
-	b = append(b, 0xff, 0xff, 0xff, 0xff)
+
+	// crc:=pat.CRC32
+	crc := CRC32(b)
+	b = append(b, byte(crc>>24), byte(crc>>16), byte(crc>>8), byte(crc))
 
 	return writer.WriteFull(b)
 }
@@ -119,6 +131,7 @@ type PMT struct { //Program Map Table
 	// Reserved4              uint8  //4bit
 	ProgramInfoLength uint16 //12bit
 	Streams           map[uint16]*PES
+	CRC32             uint32
 }
 
 func NewPMT(programNumber uint16) (pmt *PMT) {
@@ -130,21 +143,22 @@ func NewPMT(programNumber uint16) (pmt *PMT) {
 }
 
 func (pmt *PMT) Parse(reader easyio.EasyReader) (err error) {
-	b, err := reader.ReadN(12)
+	b1 := make([]byte, 12)
+	err = reader.ReadFull(b1)
 	if err != nil {
 		return fmt.Errorf("read PMT error:%w", err)
 	}
 
-	pmt.TableID = b[0]
-	pmt.SectionSyntaxIndicator = (b[1] >> 7) & 0x01
-	pmt.SectionLength = uint16(((b[1] & 0x0f) << 4) + b[2])
-	pmt.ProgramNumber = uint16(b[3]<<8 + b[4])
-	pmt.VersionNumber = (b[5] >> 1) & 0x1f
-	pmt.CurrentNextIndicator = b[5] & 0x01
-	pmt.SectionNumber = b[6]
-	pmt.LastSectionNumber = b[7]
-	pmt.PCR_PID = uint16((b[8]&0x1f)<<4 + b[9])
-	pmt.ProgramInfoLength = uint16((b[10]&0x0f)<<4 + b[11])
+	pmt.TableID = b1[0]
+	pmt.SectionSyntaxIndicator = (b1[1] & 0x80) >> 7
+	pmt.SectionLength = uint16((b1[1]&0x0f)<<8) + uint16(b1[2])
+	pmt.ProgramNumber = uint16(b1[3])<<8 + uint16(b1[4])
+	pmt.VersionNumber = (b1[5] & 0x3e) >> 1
+	pmt.CurrentNextIndicator = b1[5] & 0x01
+	pmt.SectionNumber = b1[6]
+	pmt.LastSectionNumber = b1[7]
+	pmt.PCR_PID = uint16(b1[8]&0x1f)<<8 + uint16(b1[9])
+	pmt.ProgramInfoLength = uint16((b1[10]&0x0f)<<8 + b1[11])
 
 	if pmt.TableID != 0x02 {
 		return fmt.Errorf("invalid TableID of PMT, TableID:%d", pmt.TableID)
@@ -165,35 +179,69 @@ func (pmt *PMT) Parse(reader easyio.EasyReader) (err error) {
 		return fmt.Errorf("invalid ProgramInfoLength of PMT, ProgramInfoLength:%d", pmt.ProgramInfoLength)
 	}
 
-	b, err = reader.ReadN(uint32(pmt.ProgramInfoLength))
+	b2 := make([]byte, pmt.ProgramInfoLength)
+	err = reader.ReadFull(b2)
 	if err != nil {
 		return fmt.Errorf("read descriptor of PMT error:%w", err)
 	}
 
-	b, err = reader.ReadN(uint32(pmt.SectionLength - 9 - pmt.ProgramInfoLength - 4)) //4:CRC_32
+	b3 := make([]byte, pmt.SectionLength-9-pmt.ProgramInfoLength-4) //4:CRC_32
+	err = reader.ReadFull(b3)
 	if err != nil {
 		return fmt.Errorf("read streams of PMT error:%w", err)
 	}
-	for i := 0; i+5 <= len(b); i += 5 {
-		streamPID := uint16((b[i+1]&0x1f)<<8 + b[i+2])
-		if stream, ok := pmt.Streams[streamPID]; !ok {
-			pes := NewPES(b[i], streamPID, uint16((b[i+3]&0x0f)<<8+b[i+4]))
-
-			if (pes.ESInfoLength>>10)&0x03 != 0x00 {
-				return fmt.Errorf("invalid ESInfoLength of PMT, ESInfoLength:%d", pes.ESInfoLength)
+	for i := 0; i+5 <= len(b3); i += 5 {
+		streamPID := uint16(b3[i+1]&0x1f)<<8 + uint16(b3[i+2])
+		if _, ok := pmt.Streams[streamPID]; !ok {
+			esInfoLength := uint16(b3[i+3]&0x0f)<<8 + uint16(b3[i+4])
+			if (esInfoLength>>10)&0x03 != 0x00 {
+				return fmt.Errorf("invalid ESInfoLength of PMT, ESInfoLength:%d", esInfoLength)
 			}
-
-			i += int(pes.ESInfoLength)
-			pmt.Streams[streamPID] = pes
+			i += int(esInfoLength)
+			pmt.Streams[streamPID] = NewPES(b3[i])
 		}
 	}
 
-	b, err = reader.ReadN(4) //CRC_32
+	pmt.CRC32 = CRC32(append(append(b1, b2...), b3...))
+
+	b4 := make([]byte, 4)
+	err = reader.ReadFull(b4) //CRC_32
 	if err != nil {
 		return fmt.Errorf("read CRC_32 of PMT error:%w", err)
 	}
-	//TODO: check sum of crc32
+
+	if got := binary.BigEndian.Uint32(b4); got != pmt.CRC32 {
+		return fmt.Errorf("crc error, want:%d, got:%d", pmt.CRC32, got)
+	}
 	return nil
+}
+
+func (pmt *PMT) Marshal(writer easyio.EasyWriter) (err error) {
+	b := []byte{
+		pmt.TableID,
+		((pmt.SectionSyntaxIndicator << 7) & 0x80) | 0x30 | (uint8(pmt.SectionLength>>8) & 0x0f),
+		uint8(pmt.SectionLength & 0xff),
+		uint8(pmt.ProgramNumber>>8) & 0xff,
+		uint8(pmt.ProgramNumber & 0xff),
+		0xc0 | ((pmt.VersionNumber << 1) & 0x3e) | (pmt.CurrentNextIndicator & 0x01),
+		pmt.SectionNumber,
+		pmt.LastSectionNumber,
+		0xe0 | uint8((pmt.PCR_PID>>8)&0x1f),
+		uint8(pmt.PCR_PID & 0xff),
+		0xf0 | uint8(pmt.ProgramInfoLength>>8)&0x0f,
+		uint8(pmt.ProgramInfoLength & 0xff),
+	}
+	for streamPID, stream := range pmt.Streams {
+		b = append(b, stream.StreamType)
+		b = append(b, 0xe0|uint8(streamPID>>8)&0x1f, uint8(streamPID&0xff))
+		b = append(b, 0xf0|uint8(0), uint8(0))
+	}
+
+	// crc:=pmt.CRC32
+	crc := CRC32(b)
+	b = append(b, byte(crc>>24), byte(crc>>16), byte(crc>>8), byte(crc))
+
+	return writer.WriteFull(b)
 }
 
 type CAT struct{} //Conditional Access Table, PID:0x01
---
+++ libmpeg/psi_test.go
@@ -0,0 +1,224 @@
+package libmpeg
+
+import (
+	"bytes"
+	"io/ioutil"
+	"reflect"
+	"testing"
+
+	"github.com/SmartBrave/Athena/easyio"
+)
+
+func TestPAT_Parse(t *testing.T) {
+	type args struct {
+		data []byte
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    *PAT
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+		{
+			name: "first",
+			args: args{
+				data: []byte{ //ts header: 0x47, 0x40, 0x00, 0x10, 0x00,
+					0x00, 0xb0, 0x0d, 0x00, 0x01, 0xc1, 0x00, 0x00, 0x00, 0x01, 0xf0, 0x00, 0x2a, 0xb1, 0x04, 0xb2},
+			},
+			want: &PAT{
+				TableID:                0x00,
+				SectionSyntaxIndicator: 0x01,
+				SectionLength:          0x0d,
+				TransportStreamID:      0x01,
+				VersionNumber:          0x00,
+				CurrentNextIndicator:   0x01,
+				SectionNumber:          0x00,
+				LastSectionNumber:      0x00,
+				PMTs: map[uint16]*PMT{
+					0x1000: &PMT{
+						ProgramNumber: 0x0001,
+						Streams:       make(map[uint16]*PES),
+					},
+				},
+				CRC32: 0x2ab104b2,
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		reader := easyio.NewEasyReader(bytes.NewBuffer(tt.args.data))
+		pat := NewPAT()
+
+		t.Run(tt.name, func(t *testing.T) {
+			if err := pat.Parse(reader); (err != nil) != tt.wantErr {
+				t.Errorf("PAT.Parse() error = %v, wantErr %v", err, tt.wantErr)
+			}
+			if !reflect.DeepEqual(pat, tt.want) {
+				t.Errorf("PAT.Parse()\n got pat = %+v\nwant pat = %+v", pat, tt.want)
+			}
+		})
+	}
+}
+
+func TestPAT_Marshal(t *testing.T) {
+	tests := []struct {
+		name    string
+		pat     *PAT
+		want    []byte
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+		{
+			name: "first",
+			pat: &PAT{
+				TableID:                0x00,
+				SectionSyntaxIndicator: 0x01,
+				SectionLength:          0x0d,
+				TransportStreamID:      0x01,
+				VersionNumber:          0x00,
+				CurrentNextIndicator:   0x01,
+				SectionNumber:          0x00,
+				LastSectionNumber:      0x00,
+				PMTs: map[uint16]*PMT{
+					0x1000: &PMT{
+						ProgramNumber: 0x0001,
+						Streams:       make(map[uint16]*PES),
+					},
+				},
+				CRC32: 0x2ab104b2,
+			},
+			want:    []byte{0x00, 0xb0, 0x0d, 0x00, 0x01, 0xc1, 0x00, 0x00, 0x00, 0x01, 0xf0, 0x00, 0x2a, 0xb1, 0x04, 0xb2},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			buf := bytes.NewBuffer([]byte{})
+			writer := easyio.NewEasyWriter(buf)
+			err := tt.pat.Marshal(writer)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("PAT.Marshal() error = %v, wantErr %v", err, tt.wantErr)
+			}
+
+			data, err := ioutil.ReadAll(buf)
+			if err != nil {
+				t.Errorf("ioutil.ReadAll() error = %v", err)
+			}
+			if !bytes.Equal(data, tt.want) {
+				t.Errorf("PAT.Marshal() got = %x, want %x", data, tt.want)
+			}
+		})
+	}
+}
+
+func TestPMT_Parse(t *testing.T) {
+	type args struct {
+		data []byte
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    *PMT
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+		{
+			name: "first",
+			args: args{
+				data: []byte{ //ts header: 0x47,0x50,0x00,0x10,0x00
+					0x02, 0xb0, 0x17, 0x00, 0x01, 0xc1, 0x00, 0x00, 0xe1, 0x00, 0xf0, 0x00, 0x1b, 0xe1, 0x00, 0xf0, 0x00, 0x0f, 0xe1, 0x01, 0xf0, 0x00, 0x2f, 0x44, 0xb9, 0x9b,
+				},
+			},
+			want: &PMT{
+				TableID:                0x02,
+				SectionSyntaxIndicator: 0x01,
+				SectionLength:          0x17,
+				ProgramNumber:          0x01,
+				VersionNumber:          0x00,
+				CurrentNextIndicator:   0x01,
+				SectionNumber:          0x00,
+				LastSectionNumber:      0x00,
+				PCR_PID:                0x0100,
+				ProgramInfoLength:      0x00,
+				Streams: map[uint16]*PES{
+					0x0100: &PES{
+						StreamType: 0x1b,
+					},
+					0x0101: &PES{
+						StreamType: 0x0f,
+					},
+				},
+				CRC32: 0x2f44b99b,
+			},
+		},
+	}
+	for _, tt := range tests {
+		reader := easyio.NewEasyReader(bytes.NewBuffer(tt.args.data))
+		pmt := NewPMT(0)
+
+		t.Run(tt.name, func(t *testing.T) {
+			if err := pmt.Parse(reader); (err != nil) != tt.wantErr {
+				t.Errorf("PMT.Parse() error = %v, wantErr %v", err, tt.wantErr)
+			}
+			if !reflect.DeepEqual(pmt, tt.want) {
+				t.Errorf("PMT.Parse()\n got pmt = %+v\nwant pmt = %+v", pmt, tt.want)
+			}
+		})
+	}
+}
+
+func TestPMT_Marshal(t *testing.T) {
+	tests := []struct {
+		name    string
+		pmt     *PMT
+		want    []byte
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+		{
+			name: "first",
+			pmt: &PMT{
+				TableID:                0x02,
+				SectionSyntaxIndicator: 0x01,
+				SectionLength:          0x17,
+				ProgramNumber:          0x01,
+				VersionNumber:          0x00,
+				CurrentNextIndicator:   0x01,
+				SectionNumber:          0x00,
+				LastSectionNumber:      0x00,
+				PCR_PID:                0x0100,
+				ProgramInfoLength:      0x00,
+				Streams: map[uint16]*PES{
+					0x0100: &PES{
+						StreamType: 0x1b,
+					},
+					0x0101: &PES{
+						StreamType: 0x0f,
+					},
+				},
+				CRC32: 0x2f44b99b,
+			},
+			want:    []byte{0x02, 0xb0, 0x17, 0x00, 0x01, 0xc1, 0x00, 0x00, 0xe1, 0x00, 0xf0, 0x00, 0x1b, 0xe1, 0x00, 0xf0, 0x00, 0x0f, 0xe1, 0x01, 0xf0, 0x00, 0x2f, 0x44, 0xb9, 0x9b},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			buf := bytes.NewBuffer([]byte{})
+			writer := easyio.NewEasyWriter(buf)
+			err := tt.pmt.Marshal(writer)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("PMT.Marshal() error = %v, wantErr %v", err, tt.wantErr)
+			}
+
+			data, err := ioutil.ReadAll(buf)
+			if err != nil {
+				t.Errorf("ioutil.ReadAll() error = %v", err)
+			}
+			if !bytes.Equal(data, tt.want) {
+				t.Errorf("PMT.Marshal() got = %x, want %x", data, tt.want)
+			}
+		})
+	}
+}
